71c022a5299b014de05aab0e472d43e44b8cdaab
ycpzko <50710126+ycpzko@users.noreply.github.com>
2022-02-14 11:56:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,10 +10,10 @@
 # sed -i '93s/0xf60000/0x1fb0000/g' target/
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.254/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
-sed -i 's/OpenWrt/R619ac/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/g' package/base-files/files/bin/config_generate
 
 
 
@@ -31,24 +31,24 @@ rm -rf package/lean/luci-theme-argon
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 git clone https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge
 # 更新
-# ./scripts/feeds update -a && ./scripts/feeds install -a
+ ./scripts/feeds update -a && ./scripts/feeds install -a
 
 ##########
 # Modify the version number
-sed -i "s/OpenWrt /Leopard build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /github build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Add kernel build user
-[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
-    echo 'CONFIG_KERNEL_BUILD_USER="Leopard"' >>.config ||
-    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"Leopard"@' .config
+#[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
+   # echo 'CONFIG_KERNEL_BUILD_USER="Leopard"' >>.config ||
+   # sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"Leopard"@' .config
 
 # Add kernel build domain
-[ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
-    echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
-    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
+# [ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
+   # echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
+   # sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
 
 
 # 删除lean里的百度文本（编译失败），增加百度PCS-web
